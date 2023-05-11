4cd118b6b89747c4444eecaabfb322c117d0c5b4
Vivek Upadhyay <45774184+17vivekupadhyay@users.noreply.github.com>
2023-05-11 13:56:30 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const container = document.getElementById("container");
+
+function makeRows(rows, cols) {
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+    cell.innerText = "";
+    container.appendChild(cell).className = "grid-item";
+    cell.addEventListener("click", () => {
+      cell.style.backgroundColor = "black";
+    })
+  };
+};
+
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+var y = document.getElementById("myRange").value;
+makeRows(16,16);
